cecea7cf4bc6ab2ed82f7b2bef6a61b4d699fe70
guestl <boris.nikulin@gmail.com>
2017-10-03 21:47:29 +0300

    Refactored module

--- bars.py
+++ bars.py
@@ -13,32 +13,21 @@ def load_json_from_file(file_path):
 
 
 def get_parsed_json(loaded_data):
-    seats_сount_dict = dict()
-    gps_dict = dict()
-    pure_data = dict()
-
     try:
         parsed_json_data = json.loads(loaded_data)
-    except Exception as e:
-        print("Unexpected error in json file")
-        return None, None, None
+    except ValueError:
+        print("Value error in json file")
+        return None
 
-    for item in parsed_json_data['features']:
-        seats_сount_dict[item['properties']['RowId']] = item['properties']['Attributes']['SeatsCount']
-
-        gps_dict[item['properties']['RowId']] = item['geometry']['coordinates']
-
-        pure_data[item['properties']['RowId']] = item['properties']['Attributes']
-
-    return seats_сount_dict, gps_dict, pure_data
+    return parsed_json_data['features']
 
 
-def get_biggest_bar(bars_dict):
-    return max(bars_dict.items(), key=lambda x: x[1])[0]
+def get_biggest_bar(bars_list):
+    return max(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(bars_dict):
-    return min(bars_dict.items(), key=lambda x: x[1])[0]
+def get_smallest_bar(bars_list):
+    return min(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
 def haversine(lon1, lat1, lon2, lat2):
@@ -54,18 +43,21 @@ def haversine(lon1, lat1, lon2, lat2):
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+    a_koeff = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c_koeff = 2 * asin(sqrt(a_koeff))
+    radius = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c_koeff * radius
 
 
-def get_closest_bar(bars_gps_dict, longitude, latitude):
-    distance_dict = dict()
-    for key, gps_value in bars_gps_dict.items():
-        distance_dict[key] = haversine(gps_value[0], gps_value[1], longitude, latitude)
-    return min(distance_dict.items(), key=lambda x: x[1])[0]
+def get_closest_bar(bars_list, longitude, latitude):
+    distance_list = list()
 
+    for single_bar in bars_list:
+        distance_list.append([single_bar, haversine(single_bar['geometry']['coordinates'][0],
+                             single_bar['geometry']['coordinates'][1],
+                             longitude, latitude)])
+
+    return min(distance_list, key=lambda x: x[1])[0]
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -81,11 +73,7 @@ if __name__ == '__main__':
         print("Json file '%s' must exists" % json_file_name)
         exit()
 
-    seats_сount_dict, gps_dict, pure_json_data = get_parsed_json(loaded_data)
-
-    if seats_сount_dict is None:
-        print("Something went wrong during json parsing.")
-        exit()
+    parsed_json_dict = get_parsed_json(loaded_data)
 
     try:
         user_longitude = float(input("Enter your longitude\
@@ -97,18 +85,21 @@ if __name__ == '__main__':
             \nYou have to use only digits and dots")
         exit()
 
-    smallest_bar_id = get_smallest_bar(seats_сount_dict)
-    biggest_bar_id = get_biggest_bar(seats_сount_dict)
-    closest_bar_id = get_closest_bar(gps_dict, user_latitude, user_longitude)
+    smallest_bar = get_smallest_bar(parsed_json_dict)
+    biggest_bar = get_biggest_bar(parsed_json_dict)
+    closest_bar = get_closest_bar(parsed_json_dict, user_latitude, user_longitude)
 
     print("Smallest bar is %s located in %s and with %s seats" % 
-    (pure_json_data[smallest_bar_id]['Name'], pure_json_data[smallest_bar_id]['Address'],
-    pure_json_data[smallest_bar_id]['SeatsCount']))
+    (smallest_bar['properties']['Attributes']['Name'], 
+    smallest_bar['properties']['Attributes']['Address'],
+    smallest_bar['properties']['Attributes']['SeatsCount']))
 
     print("Biggest bar is %s located in %s and with %s seats" % 
-    (pure_json_data[biggest_bar_id]['Name'], pure_json_data[biggest_bar_id]['Address'],
-    pure_json_data[biggest_bar_id]['SeatsCount']))
+    (biggest_bar['properties']['Attributes']['Name'], 
+    biggest_bar['properties']['Attributes']['Address'],
+    biggest_bar['properties']['Attributes']['SeatsCount']))
 
-    print("Closest bar for user is %s located in %s and with %s seats" % 
-    (pure_json_data[closest_bar_id]['Name'], pure_json_data[closest_bar_id]['Address'],
-    pure_json_data[closest_bar_id]['SeatsCount']))
+    print("Closest bar is %s located in %s and with %s seats" % 
+    (closest_bar['properties']['Attributes']['Name'], 
+    closest_bar['properties']['Attributes']['Address'],
+    closest_bar['properties']['Attributes']['SeatsCount']))
