72bacc4724dad14317cc6e989b4b7bf7d5c05f3e
guestl <boris.nikulin@gmail.com>
2017-10-08 11:30:16 +0300

    pep8 + small refactoring

--- bars.py
+++ bars.py
@@ -5,6 +5,7 @@ import os.path
 from math import radians, cos, sin, asin, sqrt
 import logging
 
+
 def load_json_from_file(file_path):
     if file_path:
         with codecs.open(file_path, 'r', 'utf8') as f:
@@ -16,18 +17,20 @@ def get_parsed_json(loaded_data):
     try:
         parsed_json_data = json.loads(loaded_data)
     except ValueError:
-        print("Value error in json file")
+        logging.error("Value error in json file")
         return None
 
     return parsed_json_data['features']
 
 
 def get_biggest_bar(bars_list):
-    return max(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+    return max(bars_list,
+               key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(bars_list):
-    return min(bars_list, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+    return min(bars_list,
+               key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
 def haversine(lon1, lat1, lon2, lat2):
@@ -45,23 +48,25 @@ def haversine(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a_koeff = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c_koeff = 2 * asin(sqrt(a_koeff))
-    radius = 6371  # Radius of earth in kilometers. Use 3956 for miles
-    return c_koeff * radius
+
+    earth_radius = 6371  # in kilometers. Use 3956 for miles
+    return c_koeff * earth_radius
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    distance_list = [([single_bar, haversine(single_bar['geometry']['coordinates'][0],
-                             single_bar['geometry']['coordinates'][1],
-                             longitude, latitude)]) for single_bar in bars_list]
+    distance_list = [([single_bar, haversine(
+        single_bar['geometry']['coordinates'][0],
+        single_bar['geometry']['coordinates'][1],
+        longitude, latitude)]) for single_bar in bars_list]
 
     return min(distance_list, key=lambda x: x[1])[0]
 
 
 def get_print_bar_string(single_bar, str_bar_property):
     return str_bar_property + " bar is {} located in {} and with {} seats".\
-    format(single_bar['properties']['Attributes']['Name'], 
-    single_bar['properties']['Attributes']['Address'],
-    single_bar['properties']['Attributes']['SeatsCount'])
+        format(single_bar['properties']['Attributes']['Name'],
+               single_bar['properties']['Attributes']['Address'],
+               single_bar['properties']['Attributes']['SeatsCount'])
 
 
 if __name__ == '__main__':
@@ -80,6 +85,10 @@ if __name__ == '__main__':
 
     parsed_json_dict = get_parsed_json(loaded_data)
 
+    if parsed_json_dict is None:
+        logging.error("Empty json data")
+        exit()
+
     try:
         user_longitude = float(input("Enter your longitude\
          \n(for example: 1.234567890):"))
@@ -92,7 +101,8 @@ if __name__ == '__main__':
 
     smallest_bar = get_smallest_bar(parsed_json_dict)
     biggest_bar = get_biggest_bar(parsed_json_dict)
-    closest_bar = get_closest_bar(parsed_json_dict, user_latitude, user_longitude)
+    closest_bar = get_closest_bar(
+        parsed_json_dict, user_latitude, user_longitude)
 
     print(get_print_bar_string(smallest_bar, 'Smallest'))
     print(get_print_bar_string(biggest_bar, 'Biggest'))
